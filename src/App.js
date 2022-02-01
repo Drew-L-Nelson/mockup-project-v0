1d76da9dfcc3c68eedb6b22763ec4e7110132b90
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-01 15:20:03 -0800

    Created HomePage and Feature and connected to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomePage from './components/HomePage';
 
 function App () {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Mouckup Project v0</h1>
+        <HomePage />
       </header>
     </div>
   );
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Feature from "./Feature";
+
+const HomePage = () => {
+  return(
+    <div>
+      HomePage
+      <Feature />
+    </div>
+  )
+}
+
+export default HomePage;
