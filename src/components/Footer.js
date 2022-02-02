89f7fd39b5d0fe6832e727275c769571d3edba27
Andrew Nelson <realseal177@gmail.com>
2022-02-02 12:08:42 -0800

    border and css file and links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
@@ -12,12 +14,22 @@ const Footer = () => {
                 </div>
 
                 <div className='contact-info'>
-
+                    <address>
+                        <a className='footer-phone' href='tel:+12065588968'>(206) 558-8968</a>
+                        <a className='footer-email' href='mailto:jim@rock.com'>Darci123@darci.com</a>
+                    </address>
                 </div>
             </div>
 
             <div className='footer-nav-links'>
-
+                <Router>
+                    <div className='footer-links'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/Service'>Services</Link>
+                        <Link to='/About'>About Me</Link>
+                        <Link to='/Contact'>Contact Me</Link>
+                    </div>
+                </Router>
             </div>
         </div>
     )
