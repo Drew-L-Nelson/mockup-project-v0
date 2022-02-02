ca7e30b062efd97488a59d1e36069b7dfe28b328
Andrew Nelson <realseal177@gmail.com>
2022-02-02 12:53:16 -0800

    flexboxing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import '../styles/Footer.css';
 
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className='footer-main-div'>
             <div className='footer-social-media'>
                 <div className='icons'>
-                    <a href='https://www.facebook.com/darci.kelts/'><BsFacebook color='blue' size='40px'/></a>
-                    <a href='https://www.linkedin.com/in/DarciKelts/'><BsTwitter color='blue' size='40px'/></a>
+                    <a href='https://www.facebook.com/DarciKeltsRealEstate'><BsFacebook color='blue' size='40px'/></a>
+                    <a href='https://www.instagram.com/darcikeltsrealestate/'><BsInstagram color='blue' size='40px'/></a>
                     <a href='https://www.linkedin.com/in/DarciKelts/'><BsLinkedin color='blue' size='40px'/></a>
                 </div>
             </div>
