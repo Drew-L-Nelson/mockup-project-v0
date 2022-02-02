d8df0ce39c1780b6c822e019d6a8234b72023a49
Andrew Nelson <realseal177@gmail.com>
2022-02-01 16:23:11 -0800

    footer divs created

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer-main-div'>
+            <div className='footer-social-media'>
+                <div className='icons'>
+                    
+                </div>
+
+                <div className='contact-info'>
+
+                </div>
+            </div>
+
+            <div className='footer-nav-links'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
