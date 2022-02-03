38564458bd10256f0fa9cfc726a277318d5ddf36
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-03 11:49:52 -0800

    Finished Mission component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Card, CardTitle, CardText } from 'reactstrap';
+import '../styles/Mission.css';
 
 const Mission = () => {
   return(
     <div>
-      Mission
+      <Card
+        body
+        style={{
+          backgroundColor: '#C2C5AA',
+          borderColor: '#C2C5AA',
+          width: '100%'
+        }}
+      >
+        <CardTitle tag="h5" className='Mission-card-title'>
+          Our Mission Statement
+        </CardTitle>
+        <CardText className='Mission-card-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum architecto similique nulla eligendi reprehenderit saepe quae. Facere similique provident, ducimus, architecto laudantium, in voluptatum iure mollitia perspiciatis assumenda tempore nulla. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nostrum corrupti numquam voluptas voluptatibus velit dignissimos modi possimus. Debitis, beatae. Sapiente minus fugiat possimus voluptas dolore aut quos cupiditate illo!
+        </CardText>
+      </Card>
     </div>
   )
 }
